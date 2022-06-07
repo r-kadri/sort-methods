833bddaf5d84ea2c0a2c2665631d03cfc1541afa
Ryan Kadri <ryankadri.dev@gmail.com>
2022-06-07 16:13:02 +0200

    init

---
+++ AskNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AskNumbers {
+
+    public static int[] ask() {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] mix = new int[10];
+        System.out.println("Entrez une suite de 10 nombres : ");
+
+        for(int i=0; i<10; i++) {
+            mix[i] = scanner.nextInt();
+        }
+
+        System.out.println("----------------------------");
+
+        return mix;
+    }
+
+}
